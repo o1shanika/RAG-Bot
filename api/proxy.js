8defc9cc952c79f4562e3f010f7fded48c190b0b
K_Shanika_Dilrukshi_ <116502502+o1shanika@users.noreply.github.com>
2025-08-15 10:25:58 +0530

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,16 +1,21 @@
 export default async function handler(req, res) {
   try {
+    console.log("Request body:", req.body);
+
     const response = await fetch(
-      'https://shanika2001.app.n8n.cloud/webhook/a656cdd0-a353-441b-9024-5ec364a63a71/chat'
+      'https://shanika2001.app.n8n.cloud/webhook/98d25a9d-5763-4419-91bd-ee1d2675c924',
       {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(req.body),
       }
     );
+
     const data = await response.json();
     res.status(200).json(data);
   } catch (err) {
+    console.error("Proxy error:", err);
     res.status(500).json({ error: err.message });
   }
 }
+
